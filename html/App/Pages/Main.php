cc29411a1bd5b7bb5858b788ab08fdbe5927f4a0
Krzysztof Chrościński <mchaltplay@gmail.com>
2022-07-11 15:13:28 +0200

    main page template

---
+++ html/App/Pages/Main.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Pages;
+
+class Main extends Base
+{
+	
+	public function prepareData()
+	{
+		
+	}
+}
---
+++ html/Views/Pages/Main/Main.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Views\Pages\Main;
+
+class Main extends \Views\Pages\Base
+{
+	public string $title = "Cookbook :: Strona główna";
+}
